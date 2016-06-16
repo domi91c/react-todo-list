9f0ea5498ae055ad06c35ea8e853fbc262e0159f
domi91c <dominic.n@me.com>
2016-06-16 18:35:48 -0400

    initial commit

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div> This is a React app.</div>
+    )
+  }
+
+}
+
+
+export default App;
